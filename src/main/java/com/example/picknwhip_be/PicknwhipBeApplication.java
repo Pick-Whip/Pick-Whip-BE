ffe142724a017e518b01f5093c37000efabd377c
m2 <orum1223@dgu.ac.kr>
2026-01-03 14:22:14 +0900

    first commit

---
+++ src/main/java/com/example/picknwhip_be/PicknwhipBeApplication.java
@@ -0,0 +1,12 @@
+package com.example.picknwhip_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicknwhipBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PicknwhipBeApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/picknwhip_be/PicknwhipBeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.picknwhip_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicknwhipBeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
