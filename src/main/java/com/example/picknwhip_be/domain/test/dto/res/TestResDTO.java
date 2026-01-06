bcf01d90f0c8cbc23f60bee9c84d089ba32d8c07
m2 <orum1223@dgu.ac.kr>
2026-01-06 12:43:45 +0900

    [SETTING]lint 자동화 설정

--- src/main/java/com/example/picknwhip_be/domain/test/dto/res/TestResDTO.java
+++ src/main/java/com/example/picknwhip_be/domain/test/dto/res/TestResDTO.java
@@ -5,10 +5,9 @@ import lombok.Getter;
 
 public class TestResDTO {
 
-
-            @Builder
-            @Getter
-            public static class Exception {
-            private String testString;
+  @Builder
+  @Getter
+  public static class Exception {
+    private String testString;
   }
 }
