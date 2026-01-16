f602275eee9b656f495e6212d0685e095cfad102
SeungyeonHa <0727ha@naver.com>
2026-01-17 06:31:25 +0900

    [Fix/#14] 포맷팅 수정

--- src/main/java/com/example/picknwhip_be/domain/review/entity/mapping/ReviewReply.java
+++ src/main/java/com/example/picknwhip_be/domain/review/entity/mapping/ReviewReply.java
@@ -4,7 +4,6 @@ import com.example.picknwhip_be.domain.review.entity.Review;
 import com.example.picknwhip_be.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDateTime;
 
 @Entity
