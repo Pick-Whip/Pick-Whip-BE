aa6412ffdb08c8614ec70166aaff345b3a9e08eb
miro-oss <roxkstar@isieun-ui-iMac.local>
2026-01-17 23:33:53 +0900

    코드 수정

--- src/main/java/com/example/picknwhip_be/domain/report/repository/ReportRepository.java
+++ src/main/java/com/example/picknwhip_be/domain/report/repository/ReportRepository.java
@@ -3,4 +3,9 @@ package com.example.picknwhip_be.domain.report.repository;
 import com.example.picknwhip_be.domain.report.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReportRepository extends JpaRepository<Report, Long> {}
+public interface ReportRepository extends JpaRepository<Report, Long> {
+  boolean existsByReporterAndTargetIdAndTargetType(
+      com.example.picknwhip_be.domain.user.entity.User reporter,
+      Long targetId,
+      com.example.picknwhip_be.domain.report.entity.ReportTargetType targetType);
+}
