ed5de5b778dc74a82a534773a6ce1bf928cd0728
miro-oss <roxkstar@isieun-ui-iMac.local>
2026-01-22 22:05:51 +0900

    코드 수정

--- src/main/java/com/example/picknwhip_be/domain/user/dto/req/UserRequestDTO.java
+++ src/main/java/com/example/picknwhip_be/domain/user/dto/req/UserRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.picknwhip_be.domain.user.dto.req;
 
 import com.example.picknwhip_be.domain.user.entity.WithdrawalReason;
+import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,6 +23,8 @@ public class UserRequestDTO {
   public static class ExtraInfoDTO {
     private String name;
     private String phone;
+
+    @Pattern(regexp = "^\\d{7}$", message = "생년월일은 숫자 7자리(YYMMDDG) 형식이어야 합니다.")
     private String birthdate;
   }
 
