e3e1ef69370c76fd96d959dce92311f31f064c40
m2 <orum1223@dgu.ac.kr>
2026-01-24 01:49:30 +0900

    [FEAT/#45]스웨거 명세 수정

--- src/main/java/com/example/picknwhip_be/domain/custom/controller/CustomController.java
+++ src/main/java/com/example/picknwhip_be/domain/custom/controller/CustomController.java
@@ -32,7 +32,7 @@ public class CustomController {
     return ApiResponse.of(GeneralSuccessCode.CREATED, customCommandService.saveCustom(dto));
   }
 
-  @Operation(summary = "내 보관함 목록 조회", description = "로그인한 사용자의 임시저장된 커스텀 케이크 목록을 조회합니다.")
+  @Operation(summary = "내 임시저장 목록 조회", description = "로그인한 사용자의 임시저장된 커스텀 케이크 목록을 조회합니다.")
   @GetMapping("/drafts")
   public ApiResponse<List<CustomResDTO.GetDraftListDTO>> getDraftList() {
 
@@ -42,16 +42,16 @@ public class CustomController {
     return ApiResponse.of(GeneralSuccessCode.OK, result);
   }
 
-  @Operation(summary = "보관함 상세 조회", description = "특정 임시저장 주문서(draftId)의 상세 정보를 조회합니다.")
+  @Operation(summary = "임시저장 상세 조회", description = "특정 임시저장 주문서(draftId)의 상세 정보를 조회합니다.")
   @GetMapping("/drafts/{draftId}")
   public ApiResponse<CustomResDTO.GetDraftDetailDTO> getDraftDetail(
-      @Parameter(description = "삭제할 임시저장 ID", required = true) @PathVariable Long draftId) {
+      @Parameter(description = "조회할 임시저장 ID", required = true) @PathVariable Long draftId) {
     CustomResDTO.GetDraftDetailDTO result = customQueryService.findDraftDetail(draftId);
 
     return ApiResponse.of(GeneralSuccessCode.OK, result);
   }
 
-  @Operation(summary = "보관함 내역 삭제", description = "특정 임시저장 주문서(draftId)를 삭제합니다.")
+  @Operation(summary = "임시저장 내역 삭제", description = "특정 임시저장 주문서(draftId)를 삭제합니다.")
   @DeleteMapping("/drafts/{draftId}")
   public ApiResponse<CustomResDTO.DeleteDraftDTO> deleteDraft(
       @Parameter(description = "삭제할 임시저장 ID", required = true) @PathVariable Long draftId) {
