293b9953c296c7920fcab752acc9ce09f563e9ce
m2 <orum1223@dgu.ac.kr>
2026-01-24 01:53:17 +0900

    [FEAT/#45]에러코드 수정

--- src/main/java/com/example/picknwhip_be/domain/custom/exception/code/CustomErrorCode.java
+++ src/main/java/com/example/picknwhip_be/domain/custom/exception/code/CustomErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum CustomErrorCode implements BaseErrorCode {
   DRAFT_NOT_FOUND(HttpStatus.NOT_FOUND, "DRAFT404_1", "해당 임시저장을 찾을 수 없습니다."),
   OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "OPTION404_1", "해당 옵션을 찾을 수 없습니다."),
-  FORBIDDEN(HttpStatus.NOT_FOUND, "OPTION404_2", "본인의 임시저장 디자인이 아닙니다.");
+  FORBIDDEN(HttpStatus.NOT_FOUND, "OPTION403_1", "본인의 임시저장 디자인이 아닙니다.");
 
   private final HttpStatus status;
   private final String code;
