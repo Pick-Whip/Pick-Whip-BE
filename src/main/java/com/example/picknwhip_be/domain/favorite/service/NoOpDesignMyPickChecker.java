12155c95ede2385374f87b7f0656607d6e14810c
minkyung <HaMming.bright@gmail.com>
2026-01-30 16:02:00 +0900

    [FEAT/#69] 코드리뷰 반영 - config 파일 추가 - TODO 작성

--- src/main/java/com/example/picknwhip_be/domain/favorite/service/NoOpDesignMyPickChecker.java
+++ src/main/java/com/example/picknwhip_be/domain/favorite/service/NoOpDesignMyPickChecker.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+// TODO: 추후에 마이픽 케이크 디자인 구현 후 @Primary 삭제
 @Component
 @Primary
 public class NoOpDesignMyPickChecker implements DesignMyPickChecker {
---
+++ src/main/java/com/example/picknwhip_be/global/apiPayload/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.example.picknwhip_be.global.apiPayload.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
