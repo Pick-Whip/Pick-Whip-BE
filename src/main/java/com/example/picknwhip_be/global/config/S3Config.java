ab0430e69191b18bc0b7bd02453e44ab16f57166
SeungyeonHa <0727ha@naver.com>
2026-01-31 07:40:20 +0900

    [FEAT/#89] Config 폴더를 global 아래로 이동 및 Querydsl 세팅 - Querydsl 의존성 추가 및 config 파일 작성

--- src/main/java/com/example/picknwhip_be/global/config/S3Config.java
+++ src/main/java/com/example/picknwhip_be/global/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.example.picknwhip_be.config;
+package com.example.picknwhip_be.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/example/picknwhip_be/global/config/TimeConfig.java
+++ src/main/java/com/example/picknwhip_be/global/config/TimeConfig.java
@@ -1,4 +1,4 @@
-package com.example.picknwhip_be.config;
+package com.example.picknwhip_be.global.config;
 
 import java.time.Clock;
 import org.springframework.context.annotation.Bean;
