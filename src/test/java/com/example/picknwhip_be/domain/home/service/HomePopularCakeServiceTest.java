fc8e358810c676b4cfbcc7f4d2e12416e9690654
minkyung <HaMming.bright@gmail.com>
2026-01-30 16:09:37 +0900

    [FEAT/#69] build error 수정

--- src/test/java/com/example/picknwhip_be/domain/home/service/HomePopularCakeServiceTest.java
+++ src/test/java/com/example/picknwhip_be/domain/home/service/HomePopularCakeServiceTest.java
@@ -15,7 +15,6 @@ import com.example.picknwhip_be.domain.home.entity.PopularCakeRanking;
 import com.example.picknwhip_be.domain.home.exception.HomeException;
 import com.example.picknwhip_be.domain.home.exception.code.HomeErrorCode;
 import com.example.picknwhip_be.domain.home.repository.PopularCakeRankingRepository;
-import com.example.picknwhip_be.domain.home.service.serviceImpl.HomePopularCakeServiceImpl;
 import com.example.picknwhip_be.domain.shop.entity.Shop;
 import java.util.List;
 import java.util.Set;
