178bbffa71000a52126ffe5972d1ba2bd046aa04
m2 <orum1223@dgu.ac.kr>
2026-01-31 11:50:28 +0900

    [FEAT/#92] 등록 API 완성

--- src/main/java/com/example/picknwhip_be/domain/favorite/controller/FavoriteDesignController.java
+++ src/main/java/com/example/picknwhip_be/domain/favorite/controller/FavoriteDesignController.java
@@ -1,6 +1,8 @@
 package com.example.picknwhip_be.domain.favorite.controller;
 
 import com.example.picknwhip_be.domain.favorite.dto.req.FavoriteDesignResponse;
+import com.example.picknwhip_be.domain.favorite.exception.code.FavoriteSuccessCode;
+import com.example.picknwhip_be.domain.favorite.service.FavoriteDesignService;
 import com.example.picknwhip_be.global.apiPayload.ApiResponse;
 import com.example.picknwhip_be.global.apiPayload.annotation.ExtractPayload;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,18 +16,23 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "FavoriteDesign", description = "마이픽 디자인 관련 API")
 public class FavoriteDesignController {
 
+  FavoriteDesignService favoriteDesignService;
+
   @Operation(summary = "마이픽 디자인 등록", description = "특정 디자인을 마이픽에 추가합니다.")
   @PostMapping("/{designId}/favorite")
   public ApiResponse<FavoriteDesignResponse> addFavoriteDesign(
       @PathVariable Long designId, @ExtractPayload Long userID) {
 
-    return null;
+    FavoriteDesignResponse result = favoriteDesignService.addFavoriteDesign(designId, userID);
+
+    return ApiResponse.of(FavoriteSuccessCode.FAVORITE_CREATED, result);
   }
 
   @Operation(summary = "마이픽 디자인 취소", description = "마이픽에 등록된 디자인을 삭제합니다.")
   @DeleteMapping("/{designId}/favorite")
   public ApiResponse<FavoriteDesignResponse> removeFavoriteDesign(
       @PathVariable Long designId, @ExtractPayload Long userID) {
+
     return null;
   }
 }
