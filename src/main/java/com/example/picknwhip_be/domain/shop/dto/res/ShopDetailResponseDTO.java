a88f701c5e9297db4331888f0549ffb28141a13b
minkyung <HaMming.bright@gmail.com>
2026-02-01 23:40:47 +0900

    [FEAT/#102] 가게 상세 조회 - dto 생성

---
+++ src/main/java/com/example/picknwhip_be/domain/shop/dto/res/ShopDetailResponseDTO.java
@@ -0,0 +1,19 @@
+package com.example.picknwhip_be.domain.shop.dto.res;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ShopDetailResponseDTO {
+    private Long shopId;
+    private String shopName;
+    private String shopImageUrl;
+    private Double averageRating;
+    private Integer reviewCount;
+    private String distance;
+    private String address;
+    private String phone;
+    private List<String> keywords;
+}
