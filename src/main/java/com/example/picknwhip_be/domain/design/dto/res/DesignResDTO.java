a6275cc9f60cc85aee9cb1674349fd183dd6e9b1
miro-oss <roxkstar@isieun-ui-iMac.local>
2026-02-02 17:01:36 +0900

    코드 리팩토링

--- src/main/java/com/example/picknwhip_be/domain/design/dto/res/DesignResDTO.java
+++ src/main/java/com/example/picknwhip_be/domain/design/dto/res/DesignResDTO.java
@@ -1,6 +1,6 @@
 package com.example.picknwhip_be.domain.design.dto.res;
 
-import com.example.picknwhip_be.domain.custom.dto.CustomResDTO;
+import com.example.picknwhip_be.domain.custom.dto.res.CustomResDTO;
 import com.example.picknwhip_be.domain.order.entity.enums.LetteringAlignment;
 import com.example.picknwhip_be.domain.order.entity.enums.LetteringLineCount;
 import java.util.List;
--- src/main/java/com/example/picknwhip_be/domain/design/service/query/DesignQueryServiceImpl.java
+++ src/main/java/com/example/picknwhip_be/domain/design/service/query/DesignQueryServiceImpl.java
@@ -1,11 +1,12 @@
 package com.example.picknwhip_be.domain.design.service;
 
 import com.example.picknwhip_be.domain.design.converter.DesignConverter;
-import com.example.picknwhip_be.domain.design.dto.DesignResDTO;
+import com.example.picknwhip_be.domain.design.dto.res.DesignResDTO;
 import com.example.picknwhip_be.domain.design.entity.DesignGallery;
 import com.example.picknwhip_be.domain.design.exception.DesignException;
 import com.example.picknwhip_be.domain.design.exception.code.DesignErrorCode;
 import com.example.picknwhip_be.domain.design.repository.DesignGalleryRepository;
+import com.example.picknwhip_be.domain.design.service.query.DesignQueryService;
 import com.example.picknwhip_be.domain.favorite.entity.FavoriteDesign;
 import com.example.picknwhip_be.domain.favorite.repository.FavoriteDesignRepository;
 import com.example.picknwhip_be.domain.shop.exception.ShopException;
