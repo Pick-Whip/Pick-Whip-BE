ecfee5eba2ddbc70f9efe36691c3fd4acb22ea38
miro <tldms822@naver.com>
2026-02-18 02:05:27 +0900

    Revert "[BUG] 카카오 로그인 오류"

--- src/main/java/com/example/picknwhip_be/global/apiPayload/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/picknwhip_be/global/apiPayload/config/JwtAuthenticationFilter.java
@@ -1,11 +1,9 @@
 package com.example.picknwhip_be.global.apiPayload.config;
 
 import com.example.picknwhip_be.global.apiPayload.exception.GeneralException;
-import com.example.picknwhip_be.global.apiPayload.util.CookieUtils;
 import com.example.picknwhip_be.global.apiPayload.util.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -54,9 +52,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
       return bearerToken.substring(7);
     }
-    return CookieUtils.getCookie(request, CookieUtils.ACCESS_TOKEN_COOKIE_NAME)
-        .map(Cookie::getValue)
-        .filter(StringUtils::hasText)
-        .orElse(null);
+    return null;
   }
 }
