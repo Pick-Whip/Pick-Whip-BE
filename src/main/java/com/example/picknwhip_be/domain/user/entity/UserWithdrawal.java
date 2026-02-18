c9b8548804b0c9c3b075d724fd540c30f34658b9
miro-oss <roxkstar@isieun-ui-iMac.local>
2026-02-19 00:04:34 +0900

    [BUG] 사용자 탈퇴 연관 관계 수정

--- src/main/java/com/example/picknwhip_be/domain/user/entity/UserWithdrawal.java
+++ src/main/java/com/example/picknwhip_be/domain/user/entity/UserWithdrawal.java
@@ -1,8 +1,16 @@
 package com.example.picknwhip_be.domain.user.entity;
 
 import com.example.picknwhip_be.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -15,9 +23,8 @@ public class UserWithdrawal extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long withdrawalId;
 
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id")
-  private User user;
+  @Column(name = "user_id", nullable = false)
+  private Long userId;
 
   @Column(columnDefinition = "TEXT")
   private String feedback;
